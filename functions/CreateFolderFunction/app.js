fc6bfa7347c7cb094353ac75de07dd3f0110c353
Jun Marcel Fritz <jun.fritz@gmail.com>
2020-08-07 15:55:15 -0700

    Add CreateFolderFunction code.

--- functions/CreateFolderFunction/app.js
+++ functions/CreateFolderFunction/app.js
@@ -1,5 +1,28 @@
-exports.handler = async (event, context) => {
-  console.log(JSON.stringify(event.Input, undefined, 2));
+const AWS = require("aws-sdk");
+const uuid = require('uuid');
+const s3 = new AWS.S3();
+exports.handler = async (event) => {
+  console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  // Access state input
+  const { firstName, teamName } = event.Input;
+
+  // Generate unique id and construct folder name
+  const memberId = uuid.v4();
+  const folderName = `${teamName}/${firstName}-${memberId.substr(-5)}/`;
+
+  // Construct parameters for the putObject call
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: folderName
+  };
+
+  await s3.putObject(params).promise();
+
+  // Return expected state object
+  return {
+    ...event.Input,
+    memberId,
+    folderName
+  };
 };
