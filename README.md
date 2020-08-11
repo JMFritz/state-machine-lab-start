09079c7e2cc673740b502d9e3f7eeb7dbedb3a1e
Jun Marcel Fritz <jun.fritz@gmail.com>
2020-08-10 19:20:34 -0700

    Final clean up + add readme.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Build a state machine lab (start)
+
+## Deploy this stack using the AWS SAM CLI
+
+Run the following commands in this project's root directory:
+
+```bash
+sam build
+```
+```bash
+sam deploy --guided --profile <your-aws-profile>
+```
+
+### Initial deployment in guided interactive mode
+
+In the guided interactive mode of `sam deploy` provide the following values:
+```bash
+Stack Name: state-machine-lab-start
+AWS Region: us-west-2
+Confirm changes before deploy [Y/n]: Y
+Allow SAM CLI IAM role creation [Y/n]: Y
+Save arguments to samconfig.toml [Y/n]: Y
+```
+
+## Subsequent deployments
+
+With your deployment parameters saved in your `samconfig.toml` file, you can initiate all subsequent deployments with the following command:
+
+```bash
+sam build && sam deploy
+```
--- functions/CreateFolderFunction/app.js
+++ functions/CreateFolderFunction/app.js
@@ -1,17 +1,18 @@
 const AWS = require("aws-sdk");
 const uuid = require('uuid');
 const s3 = new AWS.S3();
+
 exports.handler = async (event) => {
   console.log(JSON.stringify(event, undefined, 2));
 
-  // Access state input
-  const { firstName, teamName } = event.Input;
+  // Access your task state input
+  const { firstName, lastName, teamName } = event.Input;
 
-  // Generate unique id and construct folder name
+  // Generate ID and construct folder name
   const memberId = uuid.v4();
   const folderName = `${teamName}/${firstName}-${memberId.substr(-5)}/`;
 
-  // Construct parameters for the putObject call
+  // Construct parameters and call putObject
   const params = {
     Bucket: process.env.BUCKET_NAME,
     Key: folderName
@@ -21,7 +22,9 @@ exports.handler = async (event) => {
 
   // Return expected state object
   return {
-    ...event.Input,
+    firstName,
+    lastName,
+    teamName,
     memberId,
     folderName
   };
